8e6427c284673e5252b80af6afb47e5d3f3cfd29
Avery <trajano.juan@gmail.com>
2022-06-01 18:21:35 -0300

    Add comparison-operators.py in lesson-06 directory

---
+++ scientific-computing-with-python/python-for-everybody/lesson-06-conditional-steps/comparison-operators.py
@@ -0,0 +1,20 @@
+x = 5
+
+if x == 5:
+  print("Equals 5")
+
+if x > 4
+  print("Greater than 4")
+
+if x >= 5
+  print("Greater than or equal to 5")
+
+if x < 6
+  print("Less than 6")
+
+if x <= 5
+  print("Less than or equal to 5")
+
+if x != 6
+  print("Not equal to 6")
+  
